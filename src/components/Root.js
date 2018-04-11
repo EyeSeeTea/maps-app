adc8e04eee2101ddb0b494c7f5efdcb1755af692
Arnau Sanchez <pyarnau@gmail.com>
2018-04-11 12:08:41 +0200

    Remove unused Router

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import App from './app/App';
-import { Route } from 'react-router'
 import { IntlProvider, addLocaleData } from 'react-intl';
 import history from '../store/history';
 
